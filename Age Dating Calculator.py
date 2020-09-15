b7658886b373a70baa3964d5ab373d6a3107cd94
Garrett Burns <garehburns@users.noreply.github.com>
2020-09-15 12:11:17 -0500

    Add files via upload

---
+++ Age Dating Calculator.py
@@ -0,0 +1,13 @@
+age = 0
+
+print ("Welcome to the Age Dating Calculator!")
+print ("Type your age to find out what ages you can date \nwithout being considered a 'cradle-rocker'")
+print ()
+
+age += int(input("Age here: "))
+
+date = (age/2) + 7
+
+print (date)
+
+input ("Press 'Enter' to exit")
